feccc0f0e1a852ea13cdd1b522cae42bcd177808
DillaHiro <dillabraveshine@gmail.com>
2019-04-05 21:34:20 +0700

    coba spp

--- siswa/spp.php
+++ siswa/spp.php
@@ -10,7 +10,13 @@
     <?php include "../process/getLoginData.php" ?>
     <?php include "../component/siswa/sidebaropen.php" ?>
 
-    ini spp
+    <div class="jumbotron">
+        <h3 class="card-title"><i class="fas fa-file-invoice"> Pembayaran SPP</i></h3>
+        <p>Pembayaran SPP sekarang tidak perlu ribet lagi, tinggal klik 'selesai' beres deh :)</p>
+        <div class="container">
+            <p>Some things. . .</p>
+        </div>
+    </div>
 
     <?php include "../component/siswa/sidebarclose.php" ?>
     <?php include "../component/scripts.php" ?>
